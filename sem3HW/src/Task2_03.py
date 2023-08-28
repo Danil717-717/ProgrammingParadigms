ac67e3ec536516bdd60a463407e88d589ccdb31f
danil <danila_monster@mail.ru>
2023-08-28 10:47:50 +0300

    com17

--- sem3HW/src/Task2_03.py
+++ sem3HW/src/Task2_03.py
@@ -1,25 +1,25 @@
 
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 
 
-class Sort():
+class Sort(ABC):
     @abstractmethod
-    def sort(self):
+    def sort(self, nums):
         pass
 
 class BubbleSort(Sort):
-    def sort(nums):
+    def sort(self, nums):
         n=1
         while n < len(nums):
             for i in range(len(nums)-n):
                 if nums[i] > nums[i+1]:
                     nums[i], nums[i+1] = nums[i+1],nums[i]
             n+=1
-        return(nums)  
+        return nums  
 
 
 class SelectionSort(Sort):
-    def sort(nums):
+    def sort(self, nums):
         for i in range(len(nums)):
             lowest_value_index = i
             for j in range(i + 1, len(nums)):
@@ -28,18 +28,34 @@ class SelectionSort(Sort):
             nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
 
 class SortStrategy:
-    def strategy(self, strategy):
+    def __init__(self):
+        self.strategy = None
+
+    def set_strategy(self, strategy):
         self.strategy = strategy
 
     def sort(self, nums):
-        print('Result is', self.strategy.sort(nums)) #????????
-    
-    #def sort(numbers):
-     #   return
+        if self.strategy is None:
+            raise ValueError("Strategy is not set")
+        return self.strategy.sort(nums)
+
 
 array = [1, 7, 3, 48, 35, 6, 4]
 print(array)
-strateg = SortStrategy()
-strateg.strategy(BubbleSort())
-strateg.sort(array)
-print(array)
+#Создаем объекты стратегий
+bubble_sort = BubbleSort()
+selection_sort = SelectionSort()
+
+#Создаем объект для работы со стратегиями сортировки
+sort_strategy = SortStrategy()
+
+#Устанавливаем стратегию сортировки и выполняем сортировку
+sort_strategy.set_strategy(bubble_sort)
+sorted_array = sort_strategy.sort(array)
+print("Buble sort: ", sorted_array)
+
+sort_strategy.set_strategy(selection_sort)
+sorted_array = sort_strategy.sort(array)
+print("Selection Sort:", sorted_array)
+
+
